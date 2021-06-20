2a756d80c8cf4bbab3f0895af3de8d429d4e14b8
Akil Vanchu <akilwanchoo@gmail.com>
2021-06-20 12:37:36 +0530

    first commit

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  maxDate;
+
+  constructor() { }
+
+  ngOnInit() {
+    //this will make the datpicker to select only by the people who is 18 year old.
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
